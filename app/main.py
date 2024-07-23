9834fa5a54b46af79c26c21f2d1371bffa2b9ef2
Ivan Shakhman <shakhmanivan@gmail.com>
2024-07-23 14:15:55 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,8 +5,15 @@ class Car:
             clean_mark: int,
             brand: str
     ) -> None:
+        # I use this order in initializate default value for not using "else"
         self.comfort_class = comfort_class
+        if not 1 <= comfort_class <= 7:
+            self.comfort_class = 1
+
         self.clean_mark = clean_mark
+        if not 1 <= clean_mark <= 10:
+            self.clean_mark = 1
+
         self.brand = brand
 
 
@@ -19,8 +26,17 @@ class CarWashStation:
             count_of_ratings: int
     ) -> None:
         self.distance_from_city_center = distance_from_city_center
+        if not 1.0 <= distance_from_city_center <= 10.0:
+            self.distance_from_city_center = 1.0
+
         self.clean_power = clean_power
+        if not 1 <= clean_power <= 10:
+            self.clean_power = 1
+
         self.average_rating = average_rating
+        if not 1.0 <= average_rating <= 5.0:
+            self.average_rating = 1.0
+
         self.count_of_ratings = count_of_ratings
 
     def wash_single_car(self, car: Car) -> None:
@@ -37,8 +53,10 @@ class CarWashStation:
         return round(sum(income), 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class * (self.clean_power - car.clean_mark)
-                     * self.average_rating / self.distance_from_city_center, 1)
+        return round(
+            car.comfort_class * (self.clean_power - car.clean_mark)
+            * self.average_rating / self.distance_from_city_center, 1
+        )
 
     def rate_service(self, rate: int) -> None:
         self.average_rating = (
